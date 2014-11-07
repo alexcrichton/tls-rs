f8f1bf733f24134aa199d609397428cafdbbaf30
Alex Crichton <alex@alexcrichton.com>
2014-11-06 16:15:49 -0800

    Add some TODO

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,16 @@
 //! Bindings to native thread-local-storage in a variety of flavors.
 
+// TODO
+//  * too many fields are public for static initialization
+//  * the `statik` module leaks memory in the fallback implementation
+//      * maybe we get this for free with dtors implemented?
+//
+//  * the `statik` module does not support dynamic initialization
+//  * the `statik` module does not support dynamic destruction
+//      * hm, ManuallyDrop seems like it would be... perfect here.
+//
+
+
 #![feature(macro_rules, unsafe_destructor)]
 #![deny(missing_docs)]
 
